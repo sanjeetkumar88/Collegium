b7093a409f65809a49ff764d9eb35d43187e0095
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2025-05-14 22:03:53 +0530

    resolve CORS Error

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import axios from "../../utils/axios";
+import axios from "../utils/axios";
 
 const AuthContext = createContext();
 
--- Frontend/src/context/ClubContext.jsx
+++ Frontend/src/context/ClubContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import axios from "../../utils/axios";
+import axios from "../utils/axios";
 
 const ClubContext = createContext();
 
