58c502d54a59ccbe54652a8baec84a4f674e33d6
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2025-05-14 21:19:00 +0530

    resolve CORS Error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,13 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config({
+    path: './.env'
+})
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
