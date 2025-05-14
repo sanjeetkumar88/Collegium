63dd3a54d464b69777b965d1f32065417ad81d40
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2025-05-14 18:43:56 +0530

    ApiError

--- Backend/src/controllers/event.controller.js
+++ Backend/src/controllers/event.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { Club } from "../models/club.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
--- Backend/src/controllers/notes.controller.js
+++ Backend/src/controllers/notes.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import  jwt  from "jsonwebtoken";
